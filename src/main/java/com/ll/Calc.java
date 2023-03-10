0a77f89987f416bc9524ab37655c7c1d1061bc3d
wnstn7671 <wnstn7671@naver.com>
2023-03-10 15:52:50 +0900

    덧셈파트

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,17 @@
+package com.ll;
+
+public class Calc {
+
+    static int run(String exp)
+    {
+        String[] bits = exp.split(" \\+ ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+
+        return a + b;
+    }
+
+
+
+}
