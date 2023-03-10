4e1db3e7993c47fff0196efb0e449e34decca4f4
wnstn7671 <wnstn7671@naver.com>
2023-03-10 17:12:43 +0900

    10 * 10 * 10 == 1000

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,20 +2,34 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = !needToMulti;
 
-        exp = exp.replaceAll("- ", "+ - ");
+        if ( needToPlus ) {
+            exp = exp.replaceAll("- ", "+ -");
 
-        String[] bits = exp.split(" \\+ ");
+            String[] bits = exp.split(" \\+ ");
 
-        int sum = 0;
+            int sum = 0;
 
-        for (int i = 0; i < bits.length; i++) {
-            sum += Integer.parseInt(bits[i]);
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+        else if ( needToMulti ) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
         }
 
-        return sum;
-
-        //throw new RuntimeException("올바른 계산식이 아닙니다.");
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
-
