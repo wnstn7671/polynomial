79ea9d6c37763ef2d146fec86a826b89713dffea
wnstn7671 <wnstn7671@naver.com>
2023-03-10 17:07:23 +0900

    10 - 20 + 30

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,15 +2,10 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
-        String[] bits = exp.split(" \\+ ");
-        boolean needToPlus = exp.contains("+");
-        boolean needToMinus = exp.contains("-");
 
-        if (needToPlus) {
-            bits = exp.split(" \\+ ");
-        } else if (needToMinus) {
-            bits = exp.split(" \\- ");
-        }
+        exp= exp.replaceAll("- ","+ - ");
+
+        String[] bits = exp.split(" \\+ ");
 
         int a = Integer.parseInt(bits[0]);
         int b = Integer.parseInt(bits[1]);
@@ -18,14 +13,10 @@ public class Calc {
 
         if ( bits.length > 2 ) {
             c = Integer.parseInt(bits[2]);
-        }
 
-        if (needToPlus) {
-            return a + b+ c ;
-        } else if (needToMinus) {
-            return a - b - c;
         }
+        return a + b + c;
 
-        throw new RuntimeException("올바른 계산식이 아닙니다.");
+        //throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
