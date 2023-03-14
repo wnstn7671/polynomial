343e340d2ead3718f02c52303a77f9e972973415
wnstn7671 <wnstn7671@naver.com>
2023-03-14 17:00:13 +0900

    - ì 어렵다

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class Calc {
     public static int run(String exp) {
+        exp = exp.trim();
         exp = stripOuterBrackets(exp);
 
         // 단일항이 입력되면 바로 리턴
@@ -12,10 +13,33 @@ public class Calc {
 
         boolean needToMulti = exp.contains(" * ");
         boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
-
         boolean needToCompound = needToMulti && needToPlus;
+        boolean needToSplit =  exp.contains("(") || exp.contains(")");
 
-        if ( needToCompound ) {
+        if ( needToSplit ) {
+            int bracketsCount = 0;
+            int splitPointIndex = -1;
+
+            for ( int i = 0; i < exp.length(); i++ ) {
+                if ( exp.charAt(i) == '(' ) {
+                    bracketsCount++;
+                }
+                else if ( exp.charAt(i) == ')' ) {
+                    bracketsCount--;
+                }
+
+                if ( bracketsCount == 0 ) {
+                    splitPointIndex = i;
+                    break;
+                }
+            }
+
+            String firstExp = exp.substring(0, splitPointIndex + 1);
+            String secondExp = exp.substring(splitPointIndex + 4);
+
+            return Calc.run(firstExp) + Calc.run(secondExp);
+        }
+        else if ( needToCompound ) {
             String[] bits = exp.split(" \\+ ");
 
             String newExp = Arrays.stream(bits)
