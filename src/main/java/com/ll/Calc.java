15f50940d3d37c191806b8db8aae325e33a520f2
wnstn7671 <wnstn7671@naver.com>
2023-03-14 16:40:58 +0900

    -ìì불여러개의 괄호 제거

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 public class Calc {
     public static int run(String exp) {
-        // 단일항이 입력되면 바로 리턴
         exp = stripOuterBrackets(exp);
+
+        // 단일항이 입력되면 바로 리턴
         if ( !exp.contains(" ") ) return Integer.parseInt(exp);
 
         boolean needToMulti = exp.contains(" * ");
@@ -51,11 +52,16 @@ public class Calc {
 
         throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
+
     private static String stripOuterBrackets(String exp) {
-        if ( exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')' ) {
-            exp = exp.substring(1, exp.length() - 1);
+        int outerBracketsCount = 0;
+
+        while ( exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')' ) {
+            outerBracketsCount++;
         }
 
-        return exp;
+        if ( outerBracketsCount == 0 ) return exp;
+
+        return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
     }
 }
--- src/test/java/com/ll/CalcTests.java
+++ src/test/java/com/ll/CalcTests.java
@@ -113,4 +113,15 @@ public class CalcTests {
     void t18() {
         assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
     }
+    @Test
+    @DisplayName("((10 + 20)) == 30")
+    void t19() {
+        assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(((10 + 20))) == 30")
+    void t20() {
+        assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
+    }
 }
